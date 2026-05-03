aea9a8031ab1ca4f6b33da34acb5c3cd850ef89a
Arthur <18201736907@163.com>
2026-05-03 16:44:36 +1200

    remove monitoring

--- backend-dotnet/BackendApi/Program.cs
+++ backend-dotnet/BackendApi/Program.cs
@@ -8,7 +8,7 @@ Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 // Connect to Azure Application Insights for telemetry monitoring
-builder.Services.AddApplicationInsightsTelemetry();
+// builder.Services.AddApplicationInsightsTelemetry();
 
 // 2. Instruct .NET to read configurations from environment variables
 builder.Configuration.AddEnvironmentVariables();
